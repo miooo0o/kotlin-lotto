ac09d3e4285f37577ad4cd6e6a569d7940bb6062
Mina Kim <mina.kim@deliveryhero.com>
2025-07-20 21:27:02 +0200

    chore(application): remove unnecessary comment

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -6,13 +6,6 @@ import lotto.dto.RankedTickets
 import lotto.view.OutputView
 import kotlin.system.exitProcess
 
-/**
- * This app is a Lotto app that receives an amount from the user to purchase Lotto tickets.
- * The app waits for the winning numbers from the week and a bonus number, and with that
- * it will generate all the winning statistics for the user.
- *
- * @exception Exception to protect in case of unexpected behavior.
- */
 fun main() {
     try {
         val purchaseResult = PurchasesController.getPurchaseResult()
