1ba9b03dd5b143cf39ebf4fe5645ae16c2290748
Mina Kim <mina.kim@deliveryhero.com>
2025-07-20 21:25:42 +0200

    refactor(validator): implement new validation to number, separate purchase exception case

--- src/main/kotlin/lotto/exceptions/Validator.kt
+++ src/main/kotlin/lotto/exceptions/Validator.kt
@@ -1,9 +1,15 @@
 package lotto.exceptions
 
 import lotto.domain.LottoNumber
+import lotto.domain.LottoNumber.Companion.MAX_RANGE_NUMBER
+import lotto.domain.LottoNumber.Companion.MIN_RANGE_NUMBER
 import lotto.domain.LottoTicket
 
 object Validator {
+    fun number(num: Int) {
+        require(num in MIN_RANGE_NUMBER..MAX_RANGE_NUMBER) { ExceptionMessage.INVALID_INPUT }
+    }
+
     fun amount(amount: Int) {
         require(amount >= LottoTicket.PRICE_OF_TICKET) { ExceptionMessage.TOO_SMALL }
         require(amount % LottoTicket.PRICE_OF_TICKET == 0) { ExceptionMessage.NOT_DIVISIBLE }
@@ -15,7 +21,8 @@ object Validator {
     ) {
         val ableToPurchase = amount / LottoTicket.PRICE_OF_TICKET
         require(ableToPurchase > 0) { ExceptionMessage.TOO_SMALL }
-        require(size > 0 && size <= ableToPurchase) { ExceptionMessage.TOO_SMALL }
+        require(size > 0) { ExceptionMessage.TOO_SMALL }
+        require(size <= ableToPurchase) { ExceptionMessage.TOO_BIG }
     }
 
     fun numbers(numbers: Set<Int>) {
